2680b7efaf50509f7a8a2c87b8e98aaac692edfe
Chukwuebuka Stanley <azukachukwuebuka07@gmail.com>
2024-08-03 15:47:09 +0100

    added the mobile navbar functionality

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,7 +1,11 @@
+"use client";
 import Link from "next/link";
 import Logo from "./logo";
-
+import { useState } from "react";
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  const toggle = () => setMenu((e) => !e);
   return (
     <>
       <nav className="flex justify-between items-center py-5 text-main page-padding">
@@ -38,27 +42,30 @@ const Navbar = () => {
         <Link href="/more" className="button text-white max-lg:hidden">
           Read More
         </Link>
-        <button>
+        <button onClick={toggle} className="lg:hidden">
           <svg className="w-8" viewBox="0 0 16 11" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 10.7666V9.76656H16V10.7666H0ZM0 5.99656V4.99656H16V5.99656H0ZM0 1.22656V0.226562H16V1.22656H0Z" fill="black" />
           </svg>
         </button>
       </nav>
-      <div className="fixed top-0 left-0 w-full h-screen z-10 hidden">
+      <div className={`fixed top-0  w-full h-screen z-10 left-0 transition-transform  ${!menu && "-translate-x-full"}`}>
         <div className="relative h-full w-full">
-          <span className="bg-main/30 absolute top-0 left-0 w-screen h-screen cursor-pointer"></span>
-          <div className="bg-white absolute h-screen w-1/2 md:w-1/3 flex items-center top-0 left-0">
+          {/* <span className="bg-main/30 absolute top-0 left-0 w-screen h-screen cursor-pointer"></span> */}
+          <div className="bg-white absolute h-screen w-full md:w-1/3 gap-y-10 flex flex-col top-0 left-0">
+            <div onClick={() => setMenu(false)} className="cursor-pointer text-3xl w-fit ms-auto p-3 text-primary">
+              &#x2715;
+            </div>
             <div className="space-y-7">
-              <Link className="hover:text-primary px-2 hover:underline block" href="/">
+              <Link className="hover:text-primary px-5 border-b border-b-gray/40 pb-2 hover:underline block text-lg" href="/">
                 Home
               </Link>
-              <Link className="hover:text-primary px-2 hover:underline block" href="/">
+              <Link className="hover:text-primary px-5 border-b border-b-gray/40 pb-2 hover:underline block text-lg" href="/">
                 About
               </Link>
-              <Link className="hover:text-primary px-2 hover:underline block" href="/">
+              <Link className="hover:text-primary px-5 border-b border-b-gray/40 pb-2 hover:underline block text-lg" href="/">
                 Blog
               </Link>
-              <Link className="hover:text-primary px-2 hover:underline block" href="/">
+              <Link className="hover:text-primary px-5 border-b border-b-gray/40 pb-2 hover:underline block text-lg" href="/">
                 Contact
               </Link>
             </div>
