5dcb220a62a625e907737d20f71cac287bb89dbf
Chukwuebuka Stanley <azukachukwuebuka07@gmail.com>
2024-08-04 21:42:46 +0100

    estates page completed

---
+++ app/(frontend)/estates/page.jsx
@@ -0,0 +1,42 @@
+import Footer from "@/components/footer";
+import Listing from "@/components/listing";
+import Section from "@/components/section";
+import image1 from "@/public/firstimage.png";
+
+const estates = () => {
+  return (
+    <div className="space-y-28">
+      <Section className="space-y-16">
+        <div className="text-start">
+          <h1 className="title">
+            OUR <span className="text-primary">ESTATES</span>
+          </h1>
+          <span>Explore Our Exclusive Collection of Luxury Properties</span>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[27px]">
+          <div className="col-span-1">
+            <Listing src={image1} amount={1199000} title="Adunni Terraces" location="poka,epe" status="Sale" />
+          </div>
+          <div className="col-span-1">
+            <Listing src={image1} amount={1199000} title="Adunni Terraces" location="poka,epe" status="Sale" />
+          </div>
+          <div className="col-span-1">
+            <Listing src={image1} amount={1199000} title="Adunni Terraces" location="poka,epe" status="Sale" />
+          </div>
+          <div className="col-span-1">
+            <Listing src={image1} amount={1199000} title="Adunni Terraces" location="poka,epe" status="Sale" />
+          </div>
+          <div className="col-span-1">
+            <Listing src={image1} amount={1199000} title="Adunni Terraces" location="poka,epe" status="Sale" />
+          </div>
+          <div className="col-span-1">
+            <Listing src={image1} amount={1199000} title="Adunni Terraces" location="poka,epe" status="Sale" />
+          </div>
+        </div>
+      </Section>
+      <Footer />
+    </div>
+  );
+};
+
+export default estates;
